ab7554b0a820a632dfdd478a52b6f8528d6e47be
antimatter15 <antimatter15@gmail.com>
2023-09-12 01:02:26 -0700

    slightly less touchy controls

--- main.js
+++ main.js
@@ -1020,7 +1020,7 @@ async function main() {
 				inv = translate4(inv, -dx / innerWidth, -dy / innerHeight, 0);
 
 				let preY = inv[13];
-				inv = translate4(inv, 0, 0, 10 * (1 - dscale));
+				inv = translate4(inv, 0, 0, 3 * (1 - dscale));
 				inv[13] = preY
 
 				viewMatrix = invert4(inv);
